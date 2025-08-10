7bf807eec69a38bf76fd9e6e9b62c51a9bbf61bb
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-10 12:01:37 +0530

    Create 630.java

---
+++ Hard /630/630.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int time = 0;
+        for (int[] c : courses) {
+            time += c[0];
+            pq.offer(c[0]);
+            if (time > c[1]) time -= pq.poll();
+        }
+        return pq.size();
+    }
+}
