30269d0dccc788bedfc33894d2e9ed4e1e436b78
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-10 12:02:58 +0530

    Create 668.cpp

---
+++ Hard /668/668.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        while (left < right) {
+            int mid = left + (right - left) / 2, cnt = 0;
+            for (int i = 1; i <= m; ++i) cnt += min(mid / i, n);
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+};
