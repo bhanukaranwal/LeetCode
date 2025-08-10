72beed7f153be1c182db72b985c29e38eb1ceabc
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-10 11:49:18 +0530

    Create 330.cpp

---
+++ Hard /330/330.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int i = 0, added = 0;
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                ++added;
+            }
+        }
+        return added;
+    }
+};
